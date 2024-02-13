14c832fbd0f9ae527fa6c86bc2b8db3898868f89
GNAchoux <157470954+GNAchoux@users.noreply.github.com>
2024-02-13 22:12:10 +0900

    Title: 출석 부르기, Time: 148ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240213/출석 부르기/calling-attendance.py
@@ -0,0 +1,10 @@
+student_num = int(input())
+
+if student_num == 1:
+    print("John")
+elif student_num == 2:
+    print("Tom")
+elif student_num == 3:
+    print("Paul")
+else:
+    print("Vacancy")
