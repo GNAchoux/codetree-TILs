c959f82b4158d460e93072b8bc429df9708aeca9
GNAchoux <157470954+GNAchoux@users.noreply.github.com>
2024-02-15 23:39:06 +0900

    Title: 알파벳에 따른 평가, Time: 113ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240215/알파벳에 따른 평가/evaluation-by-alphabet.py
@@ -0,0 +1,14 @@
+rank = input()
+
+if rank == "S":
+    print("Superior")
+elif rank == "A":
+    print("Excellent")
+elif rank == "B":
+    print("Good")
+elif rank == "C":
+    print("Usually")
+elif rank == "D":
+    print("Effort")
+else:
+    print("Failure")
