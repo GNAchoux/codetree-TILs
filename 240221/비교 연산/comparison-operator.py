c39c6d14a6e8c0273f12eacc7c881f5288c41b20
GNAchoux <157470954+GNAchoux@users.noreply.github.com>
2024-02-21 19:26:19 +0900

    Title: 비교 연산, Time: 168ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240221/비교 연산/comparison-operator.py
@@ -0,0 +1,33 @@
+number = input().split()
+
+a, b = int(number[0]), int(number[1])
+
+if a >= b:
+    print(1)
+else:
+    print(0)
+
+if a > b:
+    print(1)
+else:
+    print(0)
+
+if b >= a:
+    print(1)
+else:
+    print(0)
+
+if b > a:
+    print(1)
+else:
+    print(0)
+
+if a == b:
+    print(1)
+else:
+    print(0)
+
+if a != b:
+    print(1)
+else:
+    print(0)
