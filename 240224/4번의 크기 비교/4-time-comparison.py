755031cf1eccba1730623b51c0b7cfe1b64a50d9
GNAchoux <157470954+GNAchoux@users.noreply.github.com>
2024-02-24 13:26:17 +0900

    Title: 4번의 크기 비교, Time: 138ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240224/4번의 크기 비교/4-time-comparison.py
@@ -0,0 +1,23 @@
+a = int(input())
+nums = input().split()
+b, c, d, e = int(nums[0]), int(nums[1]), int(nums[2]), int(nums[3])
+
+if a > b:
+    print(1)
+else:
+    print(0)
+
+if a > c:
+    print(1)
+else:
+    print(0)
+
+if a > d:
+    print(1)
+else:
+    print(0)
+
+if a > e:
+    print(1)
+else:
+    print(0)
