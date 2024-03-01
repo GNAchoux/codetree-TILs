a5fb68171d4617ec3b578e7481447ee223782509
GNAchoux <157470954+GNAchoux@users.noreply.github.com>
2024-03-02 08:38:56 +0900

    Title: 최댓값 구하기, Time: 136ms, Memory: 24MB, Status: Passed - CodeTree

--- 240302/최댓값 구하기/maximum-value.py
+++ 240302/최댓값 구하기/maximum-value.py
@@ -1,17 +1,17 @@
 num = input().split()
 a, b, c = int(num[0]), int(num[1]), int(num[2])
 
-if a > b:
-    if a > c:
+if a >= b:
+    if a >= c:
         print(a)
-    elif a < c and b > c:
+    elif a <= c and b >= c:
         print(b)
     else:
         print(c)
-elif a < b:
-    if b > c:
+elif a <= b:
+    if b >= c:
         print(b)
-    elif b < c and a > c:
+    elif b <= c and a >= c:
         print(a)
     else:
         print(c)
